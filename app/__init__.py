20cf9a2cf63f8bb6f0fc13937b55bb826632faf7
Marta Noya <tim4martanoya@gmail.com>
2021-11-02 07:55:35 +0100

    Process resource input JSON with and without wrapping

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,5 @@ from .kafka_consumer_configured import kafka_consumer_configured
 from .mongodb_client_configured import mongodb_client_configured
 from .process_resource import process_resource
 from .resource_audio import ResourceAudio
+from .resource_message_to_json import resource_message_to_json
 from .subtitle import Subtitle
