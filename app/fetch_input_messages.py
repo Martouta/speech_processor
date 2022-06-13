4559cccbd6c54c43517060335a4882f84da0f69e
Marta Noya <tim4martanoya@gmail.com>
2022-06-13 18:34:05 +0200

    Beautify fetch_input_messages and its tests

--- app/fetch_input_messages.py
+++ app/fetch_input_messages.py
@@ -2,6 +2,7 @@ import os
 from .json_input_resources import json_input_resources
 from .config_loaders.kafka_consumer_configured import kafka_consumer_configured
 
+
 def fetch_input_messages():
     input_file = os.getenv('INPUT_FILE')
     if input_file:
