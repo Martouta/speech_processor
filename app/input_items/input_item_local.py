f5d308a4615f9112c17a84b7f468453ac3fe336d
Marta Noya <tim4martanoya@gmail.com>
2022-12-26 04:31:28 +0100

    [Bugfix] Add extension to InputItemLocal
    Test InputItemLocal#save instead of the former InputItemLocal#download

--- app/input_items/input_item_local.py
+++ app/input_items/input_item_local.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 from .input_item import InputItem
 
@@ -6,6 +7,7 @@ class InputItemLocal(InputItem):
     def __init__(self, *, resource_id, language_code, path):
         super().__init__(resource_id=resource_id, language_code=language_code)
         self.origin_filepath = path
+        self.extension = os.path.splitext(self.origin_filepath)[1].lstrip('.')
 
     def download(self, destination_filepath):
         shutil.copyfile(self.origin_filepath, destination_filepath)
--- tests/input_items/test_input_item_local.py
+++ tests/input_items/test_input_item_local.py
@@ -1,37 +1,21 @@
 from app import InputItemLocal
-import tempfile
+import glob
 import os
-import shutil
-import pytest
+import re
 
 
 class TestInputItemLocal:
-    @pytest.fixture
-    def temp_dir(self):
-        temp_dir = tempfile.TemporaryDirectory()
-        yield temp_dir
-        temp_dir.cleanup()
+    def teardown_method(self):
+        for fname in glob.glob('resources/multimedia/test/example.txt'):
+            os.remove(fname)
 
-    def test_download(self, temp_dir):
-        origin_filepath = os.path.join(temp_dir.name, 'origin.txt')
-        destination_filepath = os.path.join(temp_dir.name, 'destination.txt')
-
-        with open(origin_filepath, 'w') as f:
-            f.write('Hello, World!')
-
-        input_item = InputItemLocal(resource_id=42, language_code='en', path=origin_filepath)
-        input_item.download(destination_filepath)
-
-        # Check that the file was copied to the destination
-        assert os.path.exists(destination_filepath)
-
-        # Check that the content of the destination file is the same as the origin file
-        with open(origin_filepath, 'r') as f:
-            origin_content = f.read()
-        with open(destination_filepath, 'r') as f:
-            destination_content = f.read()
-        assert origin_content == destination_content
-
-    @pytest.mark.skip(reason='TODO WIP')
     def test_save(self):
-        pass
+        item = InputItemLocal(
+            resource_id=42, language_code='en', path='tests/fixtures/example.txt'
+        )
+        filepath = item.save()
+
+        assert re.match(r'^' + re.escape(os.getcwd()) +
+                        r'/resources/multimedia/test/\d+-42-\d{2}-\d{2}\.\d{2}:\d{2}:\d+\.txt$', filepath)
+        with open(filepath, 'r') as file:
+            assert file.read().replace('\n', '') == 'Hello, World!'
