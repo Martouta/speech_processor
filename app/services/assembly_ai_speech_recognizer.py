d82a68749ac57f730abb53cdd293b56c09bae128
Marta Noya <tim4martanoya@gmail.com>
2023-03-21 20:09:14 +0100

    Add AssemblyAI Speech Recognizer

---
+++ app/services/assembly_ai_speech_recognizer.py
@@ -0,0 +1,90 @@
+import time
+import requests
+import os
+
+
+class AssemblyAiSpeechRecognizer:
+    UPLOAD_ENDPOINT = "https://api.assemblyai.com/v2/upload"
+    TRANSCRIPT_ENDPOINT = "https://api.assemblyai.com/v2/transcript"
+    POLLING_ENDPOINT_BASE = "https://api.assemblyai.com/v2/transcript"
+
+    @staticmethod
+    def call(filepath, _language):
+        # TODO: Add support for different languages
+        try:
+            headers = AssemblyAiSpeechRecognizer._get_headers()
+            upload_response = AssemblyAiSpeechRecognizer._upload_file(
+                filepath, headers)
+            transcript_response = AssemblyAiSpeechRecognizer._request_transcript(
+                upload_response, headers)
+            polling_endpoint = AssemblyAiSpeechRecognizer._create_polling_endpoint(
+                transcript_response)
+            AssemblyAiSpeechRecognizer._wait_for_completion(
+                polling_endpoint, headers)
+            transcribed_text = AssemblyAiSpeechRecognizer._get_transcribed_text(
+                polling_endpoint, headers)
+            print(transcribed_text)
+
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            transcribed_text = None
+
+        return transcribed_text
+
+    @staticmethod
+    def _get_headers():
+        api_key = os.environ.get('ASSEMBLYAI_API_KEY')
+        headers = {
+            'authorization': api_key,
+            "content-type": "application/json"
+        }
+        return headers
+
+    @staticmethod
+    def _upload_file(filepath, headers):
+        with open(filepath, "rb") as f:
+            # Upload the audio file to AssemblyAI
+            upload_response = requests.post(
+                AssemblyAiSpeechRecognizer.UPLOAD_ENDPOINT,
+                headers=headers, data=f.read()
+            ).json()
+        return upload_response
+
+    @staticmethod
+    def _request_transcript(upload_response, headers):
+        transcript_request = {
+            'audio_url': upload_response['upload_url']
+        }
+        transcript_response = requests.post(
+            AssemblyAiSpeechRecognizer.TRANSCRIPT_ENDPOINT,
+            json=transcript_request,
+            headers=headers
+        ).json()
+        return transcript_response
+
+    @staticmethod
+    def _create_polling_endpoint(transcript_response):
+        polling_endpoint = f"{AssemblyAiSpeechRecognizer.POLLING_ENDPOINT_BASE}/{transcript_response['id']}"
+        return polling_endpoint
+
+    @staticmethod
+    def _wait_for_completion(polling_endpoint, headers):
+        while True:
+            polling_response = requests.get(
+                polling_endpoint, headers=headers).json()
+
+            if polling_response['status'] == 'completed':
+                break
+
+            time.sleep(1)
+
+    @staticmethod
+    def _get_transcribed_text(polling_endpoint, headers):
+        paragraphs_response = requests.get(
+            polling_endpoint + "/paragraphs", headers=headers).json()
+
+        transcribed_text = ''
+        for para in paragraphs_response['paragraphs']:
+            transcribed_text += para['text'] + '\n'
+
+        return transcribed_text
---
+++ tests/services/test_assembly_ai_speech_recognizer.py
@@ -0,0 +1,87 @@
+import os
+import shutil
+import glob
+import httpretty
+from unittest.mock import patch
+from app import AssemblyAiSpeechRecognizer
+
+
+class TestAssemblyAiSpeechRecognizer:
+    def teardown_method(self):
+        for filename in glob.glob(f"{os.getcwd()}/multimedia/test/*.wav"):
+            os.remove(filename)
+
+        file_path = f"{os.getcwd()}/resources/audio_chunks/test/recognition_id"
+        if os.path.exists(file_path):
+            shutil.rmtree(file_path)
+
+    @httpretty.activate
+    @patch.dict(os.environ, {'ASSEMBLYAI_API_KEY': 'test_key'})
+    def test_transcribe_success(self):
+        audio_file_path = f"{os.getcwd()}/tests/fixtures/example.wav"
+        upload_url = "https://api.assemblyai.com/v2/upload"
+        transcript_url = "https://api.assemblyai.com/v2/transcript"
+        polling_url = "https://api.assemblyai.com/v2/transcript/test_id"
+        paragraphs_url = "https://api.assemblyai.com/v2/transcript/test_id/paragraphs"
+
+        httpretty.register_uri(
+            httpretty.POST,
+            upload_url,
+            body='{"upload_url": "https://aws.com/test_audio"}',
+            content_type="application/json"
+        )
+
+        httpretty.register_uri(
+            httpretty.POST,
+            transcript_url,
+            body='{"id": "test_id"}',
+            content_type="application/json"
+        )
+
+        httpretty.register_uri(
+            httpretty.GET,
+            polling_url,
+            responses=[
+                httpretty.Response(
+                    body='{"status": "queued"}', content_type="application/json"),
+                httpretty.Response(
+                    body='{"status": "processing"}', content_type="application/json"),
+                httpretty.Response(
+                    body='{"status": "completed"}', content_type="application/json"),
+            ]
+        )
+
+        httpretty.register_uri(
+            httpretty.GET,
+            paragraphs_url,
+            body='{"paragraphs": [{"text": "This is a test transcription."}]}',
+            content_type="application/json"
+        )
+
+        transcribed_text = AssemblyAiSpeechRecognizer.call(
+            audio_file_path, 'en-US')
+        assert transcribed_text == "This is a test transcription.\n"
+
+    @httpretty.activate
+    @patch.dict(os.environ, {'ASSEMBLYAI_API_KEY': 'test_key'})
+    def test_transcribe_failure(self):
+        audio_file_path = f"{os.getcwd()}/tests/fixtures/example.wav"
+        upload_url = "https://api.assemblyai.com/v2/upload"
+        transcript_url = "https://api.assemblyai.com/v2/transcript"
+
+        httpretty.register_uri(
+            httpretty.POST,
+            upload_url,
+            body='{"upload_url": "https://aws.com/test_audio"}',
+            content_type="application/json"
+        )
+
+        httpretty.register_uri(
+            httpretty.POST,
+            transcript_url,
+            status=500
+        )
+
+        transcribed_text = AssemblyAiSpeechRecognizer.call(
+            audio_file_path, 'en-US')
+        assert transcribed_text is None
