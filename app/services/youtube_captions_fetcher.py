14761eeeefad4083d9d767bb1dcd4bcab9e4b2dd
Marta Noya <tim4martanoya@gmail.com>
2023-03-23 18:05:29 +0100

    [Refactor] Extract YoutubeCaptionsFetcher.call into smaller methods

--- app/services/youtube_captions_fetcher.py
+++ app/services/youtube_captions_fetcher.py
@@ -9,26 +9,40 @@ from youtube_transcript_api.formatters import JSONFormatter
 class YoutubeCaptionsFetcher:
     @staticmethod
     def call(video_id, language):
-        # Get the available captions for the video
-        captions_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        captions_list = YoutubeCaptionsFetcher._get_available_captions(
+            video_id)
+        manual_captions = YoutubeCaptionsFetcher._manual_captions(
+            captions_list, language)
 
-        # Check if the desired captions exist and are not auto-generated
-        manual_captions = None
-        for transcript in captions_list:
-            if transcript.language_code == language[:2] and not transcript.is_generated:
-                manual_captions = transcript
-                break
-
-        # Raise an error if manual captions do not exist
         if manual_captions is None:
             raise ValueError(
                 f"No manual captions found for video ID {video_id} and language {language}")
 
-        # Fetch the captions and convert them to the JSON format
-        transcript_json = json.loads(
-            JSONFormatter().format_transcript(manual_captions.fetch()))
+        transcript_json = YoutubeCaptionsFetcher._captions_to_json(
+            manual_captions)
 
-        # Convert the JSON format to the Subtitle format
+        subtitle = YoutubeCaptionsFetcher._json_to_subtitles(
+            transcript_json, video_id, language)
+
+        return subtitle
+
+    @staticmethod
+    def _get_available_captions(video_id):
+        return YouTubeTranscriptApi.list_transcripts(video_id)
+
+    @staticmethod
+    def _manual_captions(captions_list, language):
+        for transcript in captions_list:
+            if transcript.language_code == language[:2] and not transcript.is_generated:
+                return transcript
+        return None
+
+    @staticmethod
+    def _captions_to_json(captions):
+        return json.loads(JSONFormatter().format_transcript(captions))
+
+    @staticmethod
+    def _json_to_subtitles(transcript_json, video_id, language):
         lines = []
         for line in transcript_json:
             start = line['start']
