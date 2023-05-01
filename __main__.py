9a4498b696226fc42ca5d8f672b9ad9ccc261bb5
Marta Noya <tim4martanoya@gmail.com>
2023-05-01 20:20:39 +0200

    [Feature] Log to file or stdout/stderr or both
    feat: Refactor logging configuration with environment-based output control
    
    - Add separate functions for creating file and standard (stdout, stderr) handlers
    - Introduce set_handler_level and configure_handler functions for better modularity
    - Update config_logs function to use the new helper functions and handle LOG_OUTPUT environment variable
    - LOG_OUTPUT supports 'file', 'standard', and 'both' options for flexible logging

--- __main__.py
+++ __main__.py
@@ -17,23 +17,57 @@ def process_threaded_inputs():
             executor.submit(app.process_resource, msg)
 
 
+def create_file_handler(logFilePath):
+    file_handler = logging.FileHandler(logFilePath)
+    return file_handler
+
+
+def create_standard_handlers():
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    return stdout_handler, stderr_handler
+
+
+def set_handler_level(handler, level, filter_func=None):
+    handler.setLevel(level)
+    if filter_func:
+        handler.addFilter(filter_func)
+
+
+def configure_handler(handler, log_output, logFormatter, root, stdout_handler=None, stderr_handler=None):
+    handler.setFormatter(logFormatter)
+    root.addHandler(handler)
+
+    if log_output in ['standard', 'both']:
+        if handler is stdout_handler:
+            set_handler_level(handler, logging.INFO,
+                              lambda record: record.levelno <= logging.INFO)
+        elif handler is stderr_handler:
+            set_handler_level(handler, logging.ERROR)
+    else:
+        set_handler_level(handler, logging.INFO)
+
+
 def config_logs():
     speech_env = os.environ['SPEECH_ENV']
+    log_output = os.environ.get('LOG_OUTPUT', 'file')
     logFilePath = f"log/{speech_env}.log"
     logFormatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     root = logging.getLogger()
 
-    handler_collection = [logging.FileHandler(logFilePath)]
-    if speech_env != 'test':
-        handler_collection.append(logging.StreamHandler(sys.stdout))
+    root.setLevel(logging.INFO)
 
-    for logging_level in [logging.ERROR, logging.INFO]:
-        root.setLevel(logging_level)
-        for handler in handler_collection:
-            handler.setLevel(logging_level)
-            handler.setFormatter(logFormatter)
-            root.addHandler(handler)
+    if log_output in ['file', 'both']:
+        file_handler = create_file_handler(logFilePath)
+        configure_handler(file_handler, log_output, logFormatter, root)
+
+    if log_output in ['standard', 'both']:
+        stdout_handler, stderr_handler = create_standard_handlers()
+        configure_handler(stdout_handler, log_output, logFormatter, root,
+                          stdout_handler=stdout_handler, stderr_handler=stderr_handler)
+        configure_handler(stderr_handler, log_output, logFormatter, root,
+                          stdout_handler=stdout_handler, stderr_handler=stderr_handler)
 
 
 if __name__ == '__main__':
